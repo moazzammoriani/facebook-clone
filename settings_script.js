8ebb4ee2efcc76db4056036967546693055d48eb
Moazzam Moriani <mozmoriani@gmail.com>
2021-08-14 22:00:55 +0500

    Removed bugs from settings_script.js

--- settings_script.js
+++ settings_script.js
@@ -16,6 +16,3 @@ getEntryNodeList("name")[3].onclick = setData("name");
 getEntryNodeList("username")[3].onclick = setData("username");
 getEntryNodeList("contact")[3].onclick = setData("contact");
 
-function generateRandUser() {
-    fetch('https://randomuser.me/api/').then((response) => return response.json() => return response.json().results)
-}
